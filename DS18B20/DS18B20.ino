efe6d9b5504ab6ab5489be3e7852261b7cdf69b4
azuluna <44519762+azuluna@users.noreply.github.com>
2019-09-22 00:24:23 +0700

    menambah ds18b20 multiple
    lebih mudah untuk digunakan dengan aktuator

---
+++ DS18B20/DS18B20.ino
@@ -0,0 +1,47 @@
+#include <OneWire.h>
+#include <DallasTemperature.h>
+
+#define ONE_WIRE_BUS 2
+
+OneWire oneWire(ONE_WIRE_BUS);  
+DallasTemperature sensors(&oneWire);
+
+int deviceCount = 0;
+float tempC;
+
+void setup(void)
+{
+  sensors.begin();  // Start up the library
+  Serial.begin(9600);
+  
+  // locate devices on the bus
+  Serial.print("Locating devices...");
+  Serial.print("Found ");
+  deviceCount = sensors.getDeviceCount();
+  Serial.print(deviceCount, DEC);
+  Serial.println(" devices.");
+  Serial.println("");
+}
+
+void loop(void)
+{ 
+  // Send command to all the sensors for temperature conversion
+  sensors.requestTemperatures(); 
+  
+  // Display temperature from each sensor
+  for (int i = 0;  i < deviceCount;  i++)
+  {
+    Serial.print("Temperature ");
+    Serial.print(i+1);
+    Serial.print("  : ");
+    tempC = sensors.getTempCByIndex(i);
+    Serial.print(tempC);
+    Serial.print((char)176);//shows degrees character
+    Serial.print("C  |  ");
+    Serial.print(DallasTemperature::toFahrenheit(tempC));
+    Serial.print((char)176);//shows degrees character
+    Serial.println("F");
+  }
+  Serial.println("");
+  delay(1000);
+}
